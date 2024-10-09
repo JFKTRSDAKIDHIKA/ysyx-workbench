1c28e212b87501f3ed4ec2dcffa97e9d2a151c56
tracer-ysyx <tracer@ysyx.org>
2024-10-09 19:48:28 +0800

    > compile NEMU ysyx_22040000 张三 Linux LAPTOP-SCVRHSJC 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  19:48:28 up  5:41,  3 users,  load average: 0.16, 0.05, 0.01

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -20,6 +20,11 @@
  */
 #include <regex.h>
 
+bool check_parentheses(int, int);
+int get_priority(char);
+int find_main_operator(int, int);
+word_t eval(int, int);
+
 enum {
   TK_NOTYPE = 256, TK_EQ,
   TK_NUM,
@@ -123,7 +128,111 @@ word_t expr(char *e, bool *success) {
   }
 
   /* TODO: Insert codes to evaluate the expression. */
-  TODO();
+  eval(0, nr_token);
+  
+
 
   return 0;
 }
+
+word_t eval(int p, int q){
+  if (p > q){
+    printf("Bad expression!\n");
+    return 0;
+  }else if (p == q){
+    if (tokens[p].type == TK_NUM){
+      return atoi(tokens[p].str); 
+    }else {
+      printf("this token should be a number.\n");
+    }
+  }else if (check_parentheses(p, q) == 1){
+    return eval(p + 1, q - 1);
+  }else {
+    int op = find_main_operator(p ,q);
+    int val1 = eval(p, op - 1);
+    int val2 = eval(op + 1, q);
+    switch (tokens[op].type) {
+      case '+': return val1 + val2;
+      case TK_MINUS : return val1 - val2;
+      case TK_MULT: return val1 * val2;
+      case TK_DIV: return val1 / val2;
+      default: assert(0); 
+  }
+  }
+  return -1;
+}
+
+int find_main_operator(int p, int q){
+  int paren_level = 0;
+  int main_op = p;
+
+  for (; p <= q; p++){
+    if (tokens[p].type == TK_LPAREN){
+      paren_level++;
+      continue;
+    }
+    
+    if (tokens[p].type == TK_RPAREN){
+      paren_level--;
+      continue;
+    }
+
+    if (tokens[p].type == TK_NUM)
+      continue;
+
+    if (paren_level != 0)
+      continue;
+
+    if (get_priority(tokens[p].str[0]) <= get_priority(tokens[main_op].str[0]))
+      main_op = p;
+  }
+  return main_op;
+}
+
+bool check_parentheses(int p, int q){
+  int sp = -1; // statck pointer.
+  int stk[32];
+  if (tokens[p].type != TK_LPAREN){
+    return 0;
+  }else {
+    sp++; // invariance: sp always points to the hightest element in the array.
+    stk[sp] = TK_LPAREN;
+    p++;
+    for(; p <= q; p++){
+      if (tokens[p].type == TK_RPAREN){
+        if (sp == -1){
+	  return 0;
+	}else {
+	  if (stk[sp] == TK_LPAREN){
+	    stk[sp] = 0;
+	    sp--;
+	  }
+	} 
+      }
+
+      if (tokens[p].type == TK_LPAREN){
+	// in case of condition : "(4 + 3) * (2 - 1)"   // false, the leftmost '(' and the rightmost ')' are not matched
+	if (sp == -1)
+	  return 0;
+        sp++;
+        stk[sp] = TK_LPAREN;
+      } 
+    }
+    if (sp == -1)
+      return 1;
+    else 
+      return 0; 
+  }
+}
+
+int get_priority(char op) {
+    if (op == '+' || op == '-') {
+        return 1;  // 加法和减法的优先级最低
+    } else if (op == '*' || op == '/') {
+        return 2;  // 乘法和除法优先级稍高
+    } else {
+        return -1; // 非运算符返回一个无效值
+    }
+}
+
+
