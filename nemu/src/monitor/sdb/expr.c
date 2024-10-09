5715ec73dc49efc7334e493d95fa9f8f224c0a3f
tracer-ysyx <tracer@ysyx.org>
2024-10-09 23:35:22 +0800

    > run NEMU ysyx_22040000 张三 Linux LAPTOP-SCVRHSJC 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  23:35:22 up  8:41,  3 users,  load average: 0.00, 0.02, 0.04

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -126,6 +126,7 @@ word_t expr(char *e, bool *success) {
     *success = false;
     return 0;
   }
+  printf("nr_token = %d\n", nr_token);
   /* TODO: Insert codes to evaluate the expression. */
   return eval(0, nr_token - 1);
   
