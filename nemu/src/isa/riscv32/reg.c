4ebdfbff3162bc92bf36cfb9dde3ab427cbe56c9
tracer-ysyx <tracer@ysyx.org>
2024-10-15 17:06:01 +0800

    > run NEMU ysyx_22040000 张三 Linux LAPTOP-SCVRHSJC 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  17:06:01 up 28 min,  1 user,  load average: 0.00, 0.03, 0.00

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -37,8 +37,8 @@ word_t isa_reg_str2val(const char *s, bool *success) {
   printf("isa_reg:%s\n", s);
   int num_regs = MUXDEF(CONFIG_RVE, 16, 32);
   for (int i = 0; i < num_regs; i++){
+    printf("regs[i]:%s, value:%d\n", regs[i], cpu.gpr[i]);
     if (strcmp(s, regs[i]) == 0){
-       printf("regs[i]:%s, value:%d\n", regs[i], cpu.gpr[i]);
        *success = 1;
        return cpu.gpr[i];
     }
