457d157c158ae6d1e70c93d942cb69a8437dfb9b
tracer-ysyx <tracer@ysyx.org>
2024-10-15 11:51:31 +0800

    > run NEMU ysyx_22040000 张三 Linux LAPTOP-SCVRHSJC 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  11:51:31 up  1:54,  1 user,  load average: 0.57, 0.21, 0.07

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -53,7 +53,8 @@ static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
     printf("监视点触发，程序暂停\n");
     nemu_state.state = NEMU_STOP;
   }  
-
+  
+  printf("bk000\n");
   if (g_print_step) { IFDEF(CONFIG_ITRACE, puts(_this->logbuf)); }
   IFDEF(CONFIG_DIFFTEST, difftest_step(_this->pc, dnpc));
 }
