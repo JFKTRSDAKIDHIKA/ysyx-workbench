3986248978fb1755ab8d635109884041e1a01ee6
tracer-ysyx <tracer@ysyx.org>
2024-12-11 13:14:05 +0800

    > run NEMU ysyx_22040000 张三 Linux LAPTOP-SCVRHSJC 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  13:14:05 up 2 days,  5:35,  0 users,  load average: 0.01, 0.02, 0.00

---
+++ nemu/tools/gen-expr/test.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <assert.h>
+#include <string.h>
+
+int main() {
+    uint32_t result = (809 + 399 * 721 * ((373 * 623 + 802 / 873 * 979)) / (238) - 989 / 607);
+    printf("Result: %u\n", result);
+    return 0;
+}
+
