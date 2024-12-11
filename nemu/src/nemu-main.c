83534f78353ca7b6eaf8425e07b8f38ca89d9d1f
tracer-ysyx <tracer@ysyx.org>
2024-12-11 13:06:37 +0800

    > run NEMU ysyx_22040000 张三 Linux LAPTOP-SCVRHSJC 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  13:06:37 up 2 days,  5:28,  0 users,  load average: 0.02, 0.02, 0.00

--- nemu/src/nemu-main.c
+++ nemu/src/nemu-main.c
@@ -1,12 +1,10 @@
 #include <common.h>
-#define MAX_TOKENS 1024  // Adjust as needed
+#define MAX_TOKENS 2048  // Adjust as needed
 			 
 
 uint32_t expr(char *e, bool *success);
 void init_monitor(int, char *[]);
 void am_init_monitor();
-void engine_start();
-int is_exit_status_bad();
 int main(int argc, char *argv[]) {
   /* Initialize the monitor. */
 #ifdef CONFIG_TARGET_AM
@@ -15,36 +13,28 @@ int main(int argc, char *argv[]) {
   init_monitor(argc, argv);
 #endif
 
-  FILE *input = fopen("/root/ysyx-workbench/nemu/tools/gen-expr/input", "r"); // Open the input file
+  char line[MAX_TOKENS];
+
+  FILE *input = fopen("/root/ysyx-workbench/nemu/tools/gen-expr/input", "r"); 
   if (input == NULL) {
     perror("Failed to open input file");
     return 1;
   }
-
-  char line[MAX_TOKENS * 2]; // Increased size for longer expressions
 			     
   while (fgets(line, sizeof(line), input) != NULL) {
-    char expression[100000];
+    char expression[62144];
     unsigned int expected_result;
-    if (sscanf(line, "%u %[^\n]", &expected_result, expression) == 2) { // Read expected result and expression
-      /*
-      size_t len = strlen(expression);					
-      if (len > 0 && expression[len - 1] == '\n'){
-	printf("最后的字符:%c\n", expression[len - 1]);
-        expression[len - 1] = '\0';
-      }
-      */
+    if (sscanf(line, "%u %[^\n]", &expected_result, expression) == 2) { 
       bool success = 1;
-       //printf("expression is %s\n", expression);
       uint32_t actual_result = expr(expression, &success);
 
       if (!success) {
-        printf("Expression evaluation failed: %s\n", expression);
+          printf("Expression evaluation failed: %s\n", expression);
       } else if (actual_result == expected_result) {
-        // printf("Test passed: %s = %u\n", expression, actual_result);
-        } else {
-        printf("Test failed: %s\nExpected: %u\nActual: %u\n", expression, expected_result, actual_result);
-        }
+          printf("One Test Passed: %s = %u\n", expression, actual_result);
+      } else {
+        printf("One Test Failed: %s\nExpected: %u\nActual: %u\n", expression, expected_result, actual_result);
+      }
     } else {
       printf("Invalid input line: %s\n", line);
       }
@@ -52,7 +42,6 @@ int main(int argc, char *argv[]) {
   }
 
   fclose(input);
-  printf("expr ok!\n");
+  printf("All tests passed!\n");
   return 0;
-  // return is_exit_status_bad();
 }
