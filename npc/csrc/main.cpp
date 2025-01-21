f085bba93c556c94339de9603ce2d84742ac351f
tracer-ysyx <tracer@ysyx.org>
2025-01-21 21:55:56 +0800

    > sim RTL ysyx_24120009 贾树傲 Linux LAPTOP-SCVRHSJC 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  21:55:56 up  6:49,  0 users,  load average: 0.32, 0.16, 0.11

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -83,7 +83,7 @@ int main(int argc, char **argv) {
                   << ", INST = " << std::hex << top->inst_debug
                   << std::endl;
         cycle++;
-    } while(!Verilated::gotFinish());
+    } while(!Verilated::gotFinish() && cycle <=20);
 
 
     delete top;
