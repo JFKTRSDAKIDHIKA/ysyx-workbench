49eaf7227d4e3ef5edf0bbf52e728e50d3550622
tracer-ysyx <tracer@ysyx.org>
2025-01-23 02:44:45 +0800

    > sim RTL ysyx_24120009 贾树傲 Linux LAPTOP-SCVRHSJC 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  02:44:45 up  8:20,  0 users,  load average: 0.13, 0.15, 0.11

--- npc/csrc/registers.cpp
+++ npc/csrc/registers.cpp
@@ -6,7 +6,7 @@ uint32_t rf_values[32];  // define the register values array
 
 void set_register_values(uint32_t rf[32]) {
     for (int i = 0; i < 32; i = i + 1) {
-        rf_values[i] = rf[i];
+        rf_values[i/2] = rf[i];
         std::cout << "Register " << i << ": 0x" << std::hex << rf[i] << std::endl;  // 打印寄存器值
     }
 }
