e60b0517cda577202e3a24ba22acb0ef82659536
tracer-ysyx <tracer@ysyx.org>
2025-01-23 02:10:24 +0800

    > sim RTL ysyx_24120009 贾树傲 Linux LAPTOP-SCVRHSJC 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  02:10:24 up  7:46,  0 users,  load average: 0.41, 0.28, 0.18

--- npc/obj_dir/Vysyx_24120009_core___024root__DepSet_h6838f9e1__0.cpp
+++ npc/obj_dir/Vysyx_24120009_core___024root__DepSet_h6838f9e1__0.cpp
@@ -572,7 +572,7 @@ VL_INLINE_OPT void Vysyx_24120009_core___024root___nba_sequent__TOP__0(Vysyx_241
     vlSelf->ysyx_24120009_core__DOT__exu__DOT__wb_mux__DOT__i1__DOT__data_list[2U] 
         = ((IData)(4U) + vlSelf->ysyx_24120009_core__DOT__pc);
     vlSelf->ysyx_24120009_core__DOT__idu__DOT__op2_sel_mux__DOT__i0__DOT__data_list[3U] 
-        = (vlSelf->ysyx_24120009_core__DOT__pc - (IData)(4U));
+        = vlSelf->ysyx_24120009_core__DOT__pc;
     vlSelf->pc_debug = vlSelf->imem_addr;
 }
 
