f4a683fc9140c26cf9edcbd078e0d2815663f80e
tracer-ysyx <tracer@ysyx.org>
2025-01-23 02:02:29 +0800

    > sim RTL ysyx_24120009 贾树傲 Linux LAPTOP-SCVRHSJC 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  02:02:29 up  7:38,  0 users,  load average: 0.31, 0.18, 0.12

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -117,7 +117,12 @@ int main(int argc, char **argv) {
           << ", Op2_debug: 0x" << top->Op2_debug 
           << ", reg_write_data: 0x" << top->reg_write_data_debug 
           << std::dec << std::endl;  // 输出完十六进制后恢复为十进制
-        std::cout << "waddr (dec): " << top->waddr_debug << std::endl;
+        if (top->waddr_debug != 0) {
+    std::cout << "waddr: 0x" << std::hex << top->waddr_debug << std::endl;
+} else {
+    std::cout << "waddr is zero" << std::endl;
+}
+
 
 
         // ref execute one instruction
