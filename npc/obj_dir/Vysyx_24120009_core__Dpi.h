30ffc936774df3b3c6df9af732e0b38e0ee3c8c8
tracer-ysyx <tracer@ysyx.org>
2025-01-23 02:08:28 +0800

    > sim RTL ysyx_24120009 贾树傲 Linux LAPTOP-SCVRHSJC 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  02:08:28 up  7:44,  0 users,  load average: 0.34, 0.28, 0.17

--- npc/obj_dir/Vysyx_24120009_core__Dpi.h
+++ npc/obj_dir/Vysyx_24120009_core__Dpi.h
@@ -16,7 +16,7 @@ extern "C" {
 
 
     // DPI IMPORTS
-    // DPI import at vsrc/common/ysyx_24120009_RegisterFile.v:20:34
+    // DPI import at vsrc/common/ysyx_24120009_RegisterFile.v:35:34
     extern void get_register_values(const svLogicVecVal* rf);
     // DPI import at vsrc/cpu/ysyx_24120009_core.v:61:34
     extern void simulation_exit();
--- npc/vsrc/common/ysyx_24120009_RegisterFile.v
+++ npc/vsrc/common/ysyx_24120009_RegisterFile.v
@@ -17,17 +17,12 @@ module ysyx_24120009_RegisterFile #(ADDR_WIDTH = 1, DATA_WIDTH = 1) (
         end
     end
 
-    import "DPI-C" function void get_register_values(input logic [31:0] rf[32]);
-    always @(posedge clk) begin
-        get_register_values(rf);  // 调用 DPI-C 函数，传递整个寄存器文件
-    end
-
     // Declare the register file (32-bit registers)
     reg [DATA_WIDTH-1:0] rf [2**ADDR_WIDTH-1:0];
 
     always @(posedge clk) begin
         if (wen && waddr != 0) begin
-            rf[5'b00010] <= wdata;
+            rf[waddr] <= wdata;
         end
         rf[0] <= {DATA_WIDTH{1'b0}}; // Ensure rf[0] is always zero
     end
@@ -36,4 +31,9 @@ module ysyx_24120009_RegisterFile #(ADDR_WIDTH = 1, DATA_WIDTH = 1) (
     assign rdata2 = rf[raddr2];
     // debug
     assign x2_debug = rf[2];
+
+    import "DPI-C" function void get_register_values(input logic [31:0] rf[32]);
+    always @(posedge clk) begin
+        get_register_values(rf);  // 调用 DPI-C 函数，传递整个寄存器文件
+    end
 endmodule
