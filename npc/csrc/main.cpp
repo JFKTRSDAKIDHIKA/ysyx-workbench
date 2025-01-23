b79904371fb84f0af7af1256f95470169751c721
tracer-ysyx <tracer@ysyx.org>
2025-01-23 16:19:07 +0800

    > sim RTL ysyx_24120009 贾树傲 Linux LAPTOP-SCVRHSJC 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  16:19:07 up 14:25,  0 users,  load average: 0.12, 0.14, 0.10

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -17,11 +17,11 @@ extern "C" void get_register_values(uint32_t rf[32]) {
     set_register_values(rf);  // set the register values
 }
 extern "C" int pmem_read(int raddr) {
-    // raddr = raddr & ~0x3u;  // 清除低两位，确保按4字节对齐
+    raddr = raddr & ~0x3u;  // 清除低两位，确保按4字节对齐
     return Memory::pmem_read(raddr);
 }
 extern "C" void pmem_write(int waddr, int wdata, char wmask) {
-    // waddr = waddr & ~0x3u;  // 清除低两位，确保按4字节对齐
+    waddr = waddr & ~0x3u;  // 清除低两位，确保按4字节对齐
     Memory::pmem_write(waddr, wdata, wmask);
 }
 
