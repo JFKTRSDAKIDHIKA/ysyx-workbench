078e5d6c9eaaefedbf53718d498a33aef16869ae
tracer-ysyx <tracer@ysyx.org>
2025-01-26 12:51:55 +0800

    > run NEMU ysyx_24120009 贾树傲 Linux LAPTOP-SCVRHSJC 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  12:51:55 up  2:16,  0 users,  load average: 0.36, 0.23, 0.18

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -159,7 +159,7 @@ static int cmd_x(char* args){
     printf("Invalid expression\n");
     return 0;
   }
-  printf("ok\n");
+  printf("Starting address: 0x%x\n", starting_addr);
   vaddr_t addr = starting_addr;
   for (; addr < starting_addr + len; addr = addr + 4){
     if (addr < 0x80000000 || addr >= 0x80000000 + CONFIG_MSIZE){
