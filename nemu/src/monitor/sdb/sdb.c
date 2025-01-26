853c760e518b246e6e26397b608de040227a2160
tracer-ysyx <tracer@ysyx.org>
2025-01-26 12:57:06 +0800

    > run NEMU ysyx_24120009 贾树傲 Linux LAPTOP-SCVRHSJC 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  12:57:06 up  2:21,  0 users,  load average: 0.34, 0.23, 0.19

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -159,13 +159,13 @@ static int cmd_x(char* args){
     printf("Invalid expression\n");
     return 0;
   }
-  vaddr_t addr = starting_addr;
-  for (; addr < starting_addr + len; addr = addr + 4){
+  vaddr_t addr = starting_addr + len;
+  for (; addr >= starting_addr; addr = addr - 4){
     if (addr < 0x80000000 || addr + len >= 0x80000000 + CONFIG_MSIZE){
       printf("0x%x is out of bound\n", addr);
       return 0;
     }
-    word_t data = vaddr_read(addr, 4); // 每次读取一行内存的数据
+    word_t data = vaddr_read(addr, 4); 
     printf("Data read from 0x%x (length %d): 0x%x\n", addr, 4, data);
   }
   return 0;
