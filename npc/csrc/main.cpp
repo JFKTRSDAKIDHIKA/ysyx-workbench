bb657cdc70f314840a3446a244d40ebb0ea769a3
tracer-ysyx <tracer@ysyx.org>
2025-01-26 10:57:24 +0800

    > sim RTL ysyx_24120009 贾树傲 Linux LAPTOP-SCVRHSJC 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  10:57:24 up 21 min,  0 users,  load average: 0.08, 0.07, 0.02

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -123,7 +123,6 @@ int check_memory(paddr_t start_addr, size_t size) {
 void tick(Vysyx_24120009_core* top, bool step_mode, bool is_reset) {
     top->clk = 0;
     top->eval();
-                print_memory(0x800001a8, 40);
     
     if (!is_reset) {
         // print some debug info when registers have yet been updated!
