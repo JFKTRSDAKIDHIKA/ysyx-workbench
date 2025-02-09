b8a977cbeb15970d0762a9695aa5f6cc28fcd9a3
tracer-ysyx <tracer@ysyx.org>
2025-02-09 19:36:57 +0800

    > run NEMU ysyx_24120009 贾树傲 Linux LAPTOP-5GG1EBB7 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  19:36:57 up  7:30,  0 users,  load average: 0.18, 0.09, 0.09

--- nemu/src/device/io/mmio.c
+++ nemu/src/device/io/mmio.c
@@ -32,6 +32,7 @@ static void report_mmio_overlap(const char *name1, paddr_t l1, paddr_t r1,
                "with %s@[" FMT_PADDR ", " FMT_PADDR "]", name1, l1, r1, name2, l2, r2);
 }
 
+#ifdef CONFIG_DTRACE
 // dtrace log function
 static void dtrace_log(const char *type, paddr_t addr, int len, word_t data, const char *name) {
   if (strcmp(type, "Read") == 0) {
@@ -40,6 +41,7 @@ static void dtrace_log(const char *type, paddr_t addr, int len, word_t data, con
     printf("[dtrace] Write to   [%s] at 0x%08x, len=%d, data=0x%08x\n", name, addr, len, data);
   }
 }
+#endif
 
 /* device interface */
 void add_mmio_map(const char *name, paddr_t addr, void *space, uint32_t len, io_callback_t callback) {
@@ -71,7 +73,9 @@ word_t mmio_read(paddr_t addr, int len) {
   }
 
   word_t data = map_read(addr, len, map);
+#ifdef CONFIG_DTRACE
   dtrace_log("Read", addr, len, data, map->name);
+#endif
 
   return data;
 }
@@ -85,5 +89,7 @@ void mmio_write(paddr_t addr, int len, word_t data) {
   
   map_write(addr, len, data, map);
 
+#ifdef CONFIG_DTRACE
   dtrace_log("Write", addr, len, data, map->name);
+#endif
 }
