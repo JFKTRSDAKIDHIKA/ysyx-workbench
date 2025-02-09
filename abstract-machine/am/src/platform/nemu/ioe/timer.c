ec8fbcdef4aafaa8c07520cc7b5744ca5dbeea9b
tracer-ysyx <tracer@ysyx.org>
2025-02-09 19:12:43 +0800

    > run NEMU ysyx_24120009 贾树傲 Linux LAPTOP-5GG1EBB7 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  19:12:43 up  7:05,  0 users,  load average: 0.07, 0.03, 0.01

--- abstract-machine/am/src/platform/nemu/ioe/timer.c
+++ abstract-machine/am/src/platform/nemu/ioe/timer.c
@@ -4,16 +4,23 @@
 static uint64_t start_time = 0;
 
 void __am_timer_init() {
-  uint32_t lo = inl(RTC_ADDR);
-  uint32_t hi = inl(RTC_ADDR + 4);
+  uint32_t hi, lo, hi2;
+  do {
+    hi = inl(RTC_ADDR + 4);  // 读取高32位
+    lo = inl(RTC_ADDR);       // 读取低32位
+    hi2 = inl(RTC_ADDR + 4);  // 再次读取高32位
+  } while (hi != hi2);        // 确保高位一致
   start_time = ((uint64_t)hi << 32) | lo;
 }
 
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
-  uint32_t lo = inl(RTC_ADDR);
-  uint32_t hi = inl(RTC_ADDR + 4);
+  uint32_t hi, lo, hi2;
+  do {
+    hi = inl(RTC_ADDR + 4);  // 读取高32位
+    lo = inl(RTC_ADDR);       // 读取低32位
+    hi2 = inl(RTC_ADDR + 4);  // 再次读取高32位
+  } while (hi != hi2);        // 确保高位一致
   uint64_t current_time = ((uint64_t)hi << 32) | lo;
-
   uptime->us = current_time - start_time;
 }
 
