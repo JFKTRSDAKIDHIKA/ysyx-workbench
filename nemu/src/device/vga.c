9476b91b35b748ceb47602318e6fe3a4cfe3c316
tracer-ysyx <tracer@ysyx.org>
2025-02-10 15:08:00 +0800

    > run NEMU ysyx_24120009 贾树傲 Linux LAPTOP-5GG1EBB7 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  15:08:00 up  3:24,  0 users,  load average: 0.50, 0.31, 0.18

--- nemu/src/device/vga.c
+++ nemu/src/device/vga.c
@@ -72,7 +72,7 @@ static inline void update_screen() {
 #endif
 #endif
 
-void vga_update_screen() {
+void vga_update_screen(uint32_t offset, int len, bool is_write) {
   if (vgactl_port_base[1] != 0) {
     printf("vga_update_screen\n");
     update_screen();
@@ -80,23 +80,14 @@ void vga_update_screen() {
   }
 }
 
-void test(uint32_t offset, int len, bool is_write) {
-  printf("test\n");
-  if (offset == 0) {
-    vga_update_screen();
-  }
-}
-
-
 void init_vga() {
   vgactl_port_base = (uint32_t *)new_space(8);
   vgactl_port_base[0] = (screen_width() << 16) | screen_height();
   vgactl_port_base[1] = 1;
-  vga_update_screen();
 #ifdef CONFIG_HAS_PORT_IO
   add_pio_map ("vgactl", CONFIG_VGA_CTL_PORT, vgactl_port_base, 8, NULL);
 #else
-  add_mmio_map("vgactl", CONFIG_VGA_CTL_MMIO, vgactl_port_base, 8, test);
+  add_mmio_map("vgactl", CONFIG_VGA_CTL_MMIO, vgactl_port_base, 8, vga_update_screen);
 #endif
 
   vmem = new_space(screen_size());
