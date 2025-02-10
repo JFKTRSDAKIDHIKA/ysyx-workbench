1f5a0690ecafdcd0d05e43c20be034b61c4e8dd8
tracer-ysyx <tracer@ysyx.org>
2025-02-10 14:15:08 +0800

    > run NEMU ysyx_24120009 贾树傲 Linux LAPTOP-5GG1EBB7 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  14:15:08 up  2:31,  0 users,  load average: 0.32, 0.13, 0.07

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -4,7 +4,15 @@
 #define SYNC_ADDR (VGACTL_ADDR + 4)
 
 void __am_gpu_init() {
+  int i;
 
+  uint32_t vga_ctl = inl(VGACTL_ADDR);
+  int w = (vga_ctl >> 16) & 0xFFFF; 
+  int h = vga_ctl & 0xFFFF; 
+
+  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+  for (i = 0; i < w * h; i++) fb[i] = 100;
+  outl(SYNC_ADDR, 1);
 }
 
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
