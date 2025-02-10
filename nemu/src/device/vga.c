e74482f5dbcd7bc2c1e5638d58b5a33cb6078fb4
tracer-ysyx <tracer@ysyx.org>
2025-02-10 13:14:38 +0800

    > run NEMU ysyx_24120009 贾树傲 Linux LAPTOP-5GG1EBB7 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  13:14:38 up  1:31,  0 users,  load average: 0.24, 0.18, 0.12

--- nemu/src/device/vga.c
+++ nemu/src/device/vga.c
@@ -73,11 +73,6 @@ static inline void update_screen() {
 
 void vga_update_screen() {
   update_screen();
-
-  if (vgactl_port_base[1] != 0) {
-    update_screen();
-    vgactl_port_base[1] = 0;
-  }
 }
 
 
