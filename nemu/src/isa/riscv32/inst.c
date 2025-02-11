340b4157c1de780630b4d8a1eef327b718af8bb9
tracer-ysyx <tracer@ysyx.org>
2025-02-11 21:23:32 +0800

    > run NEMU ysyx_24120009 贾树傲 Linux LAPTOP-5GG1EBB7 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  21:23:32 up 18:31,  0 users,  load average: 0.01, 0.06, 0.02

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -165,7 +165,9 @@ static int decode_exec(Decode *s) {
 
   // INSTPAT("0000000 00001 00000 000 00000 11100 11", ebreak , N, NEMUTRAP(s->pc, R(10))); // R(10) is $a0
   INSTPAT("??????? ????? ????? ??? ????? ????? ??", inv    , N, INV(s->pc));
-  INSTPAT("0000000 00000 00000 000 00000 11100 11", ecall  , N, mepc = s->pc; s->dnpc = mtvec; mcause = 0x8;); 
+  INSTPAT("0000000 00000 00000 000 00000 11100 11", ecall  , N, 
+    printf("ecall: pc = %x\n,  dpc = %x\n", s->pc, s->dnpc);
+    mepc = s->pc; s->dnpc = mtvec; mcause = 0x8;); 
 
   INSTPAT_END();
 
