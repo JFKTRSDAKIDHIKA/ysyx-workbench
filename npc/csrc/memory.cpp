54ced4ba41e810ba8f1e862e3a8ff531ec37c1c9
tracer-ysyx <tracer@ysyx.org>
2025-02-19 15:39:30 +0800

    > sim RTL ysyx_24120009 贾树傲 Linux LAPTOP-5GG1EBB7 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  15:39:30 up 33 min,  0 users,  load average: 0.28, 0.11, 0.03

--- npc/csrc/memory.cpp
+++ npc/csrc/memory.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 
 const uint32_t BASE_ADDR = 0x80000000;                // Base address of the memory
-const int MEMORY_SIZE = 1024 * 64;                    // 64KB memory
+const int MEMORY_SIZE = 1024 * 64 * 64;                    // 64KB memory
 std::vector<uint8_t> Memory::memory(MEMORY_SIZE, 0);  // Initialize memory with size MEMORY_SIZE and set all elements to 0
 
 
