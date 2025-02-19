c4c582cbfeacebbdc2a840ce7970b5c441556c00
tracer-ysyx <tracer@ysyx.org>
2025-02-19 15:30:43 +0800

    > sim RTL ysyx_24120009 贾树傲 Linux LAPTOP-5GG1EBB7 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  15:30:43 up 25 min,  0 users,  load average: 0.03, 0.07, 0.01

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -12,7 +12,7 @@
 #include <readline/history.h>
 #include <time.h>
 #define CLOCK_ADDRESS 0xa0000048 
-#define UART_BASE_ADDR 0xa00003f8  
+#define UART_BASE_ADDR 0xa00003F8  
 #define UART_ADDR_LEN  8          
 
 // #define ENABLE_MEMORY_CHECK 1
