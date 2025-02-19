86d4a82cd6299d8cfa1383a003c1caab8994d9c9
tracer-ysyx <tracer@ysyx.org>
2025-02-19 11:44:07 +0800

    > sim RTL ysyx_24120009 贾树傲 Linux LAPTOP-5GG1EBB7 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  11:44:07 up 52 min,  0 users,  load average: 0.06, 0.09, 0.09

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -210,125 +210,125 @@ static char* rl_gets() {
     return line_read;
   }
   
-  static int execute_single_step() {
-    tick(top, false);  
-    ref_difftest_exec(1);
-    ref_difftest_regcpy(&ref, DIFFTEST_TO_REF);
-    return check_reg(top);
+static int execute_single_step() {
+  tick(top, false);  
+  ref_difftest_exec(1);
+  ref_difftest_regcpy(&ref, DIFFTEST_TO_REF);
+  return check_reg(top);
+}
+
+static int cmd_c(char *args) {
+  while(!Verilated::gotFinish()) {
+    int ret = execute_single_step();
+    if (ret < 0) return -1;
   }
-  
-  static int cmd_c(char *args) {
-    while(!Verilated::gotFinish()) {
+  return 0;
+}
+
+static int cmd_q(char *args) {
+  return -1;
+}
+
+static int cmd_si(char* args);
+
+static int cmd_x(char* args);
+
+static int cmd_info(char* args);
+
+static struct {
+  const char *name;
+  const char *description;
+  int (*handler) (char *);
+} cmd_table [] = {
+  { "c", "Continue the execution of the program", cmd_c },
+  { "q", "Exit NPC", cmd_q },
+  { "si", "Single-step execute N instructions and then pause. If N is not provided, default to 1", cmd_si },
+  { "info", "Print registers", cmd_info},
+  { "x", "Use the expression as the starting memory address, and output N consecutive 4-byte blocks in hexadecimal", cmd_x}
+};
+
+#define NR_CMD 5
+
+static int cmd_si(char* args) {
+  char *arg = strtok(NULL, "");
+
+  if (arg == NULL) {
+      // 单步执行
       int ret = execute_single_step();
       if (ret < 0) return -1;
-    }
+  } else {
+      // 多步执行
+      int steps = atoi(arg);
+      for (int i = 0; i < steps; i++) {
+          int ret = execute_single_step();
+          if (ret < 0) return -1;
+      }
+  }
+
+  return 0;
+}
+
+static int cmd_info(char* args){
+  char *arg = strtok(NULL, "");
+
+  if (arg == NULL){
+    printf("info r : 打印寄存器状态\n");
+  } else if (strcmp(arg, "r") == 0) {
+    print_register_values();
+  } else {
+    printf("Unknown arg '%s'\n", arg);
+  }
+  return 0;
+}
+
+static int cmd_x(char* args){
+  int arg0;
+  char arg1[100]; 
+
+  if (args == NULL){
+    printf("Lack args\n");
     return 0;
   }
-
-  static int cmd_q(char *args) {
-    return -1;
-  }
-
-  static int cmd_si(char* args);
-
-  static int cmd_x(char* args);
-
-  static int cmd_info(char* args);
-
-  static struct {
-    const char *name;
-    const char *description;
-    int (*handler) (char *);
-  } cmd_table [] = {
-    { "c", "Continue the execution of the program", cmd_c },
-    { "q", "Exit NPC", cmd_q },
-    { "si", "Single-step execute N instructions and then pause. If N is not provided, default to 1", cmd_si },
-    { "info", "Print registers", cmd_info},
-    { "x", "Use the expression as the starting memory address, and output N consecutive 4-byte blocks in hexadecimal", cmd_x}
-  };
-
-  #define NR_CMD 5
-
-  static int cmd_si(char* args) {
-    char *arg = strtok(NULL, "");
-
-    if (arg == NULL) {
-        // 单步执行
-        int ret = execute_single_step();
-        if (ret < 0) return -1;
-    } else {
-        // 多步执行
-        int steps = atoi(arg);
-        for (int i = 0; i < steps; i++) {
-            int ret = execute_single_step();
-            if (ret < 0) return -1;
-        }
-    }
-
-    return 0;
-  }
-
-  static int cmd_info(char* args){
-    char *arg = strtok(NULL, "");
-  
-    if (arg == NULL){
-      printf("info r : 打印寄存器状态\n");
-    } else if (strcmp(arg, "r") == 0) {
-      print_register_values();
-    } else {
-      printf("Unknown arg '%s'\n", arg);
-    }
-    return 0;
-  }
-
-  static int cmd_x(char* args){
-    int arg0;
-    char arg1[100]; 
-  
-    if (args == NULL){
-       printf("Lack args\n");
-       return 0;
-    }
-       
-    if (sscanf(args, "%d %[^\n]", &arg0, arg1) != 2) {
-       printf("Invalid args\n");
-       return 0;
-     }
     
-    int len = 4 * arg0;
-
-    // print_memory((paddr_t)arg1, len);
-  
+  if (sscanf(args, "%d %[^\n]", &arg0, arg1) != 2) {
+    printf("Invalid args\n");
     return 0;
   }
+  
+  int len = 4 * arg0;
 
-  void sdb_mainloop() {
-    for (char *str; (str = rl_gets()) != NULL && !Verilated::gotFinish(); ) {
-      char *str_end = str + strlen(str);
-  
-      /* extract the first token as the command */
-      char *cmd = strtok(str, " ");
-      if (cmd == NULL) { continue; }
-  
-      /* treat the remaining string as the arguments,
-       * which may need further parsing
-       */
-      char *args = cmd + strlen(cmd) + 1;
-      if (args >= str_end) {
-        args = NULL;
-      }
-  
-      int i;
-      for (i = 0; i < NR_CMD; i ++) {
-        if (strcmp(cmd, cmd_table[i].name) == 0) {
-          if (cmd_table[i].handler(args) < 0) { return; }
-          break;
-        }
-      }
-  
-      if (i == NR_CMD) { printf("Unknown command '%s'\n", cmd); }
+  // print_memory((paddr_t)arg1, len);
+
+  return 0;
+}
+
+void sdb_mainloop() {
+  for (char *str; (str = rl_gets()) != NULL && !Verilated::gotFinish(); ) {
+    char *str_end = str + strlen(str);
+
+    /* extract the first token as the command */
+    char *cmd = strtok(str, " ");
+    if (cmd == NULL) { continue; }
+
+    /* treat the remaining string as the arguments,
+    * which may need further parsing
+    */
+    char *args = cmd + strlen(cmd) + 1;
+    if (args >= str_end) {
+      args = NULL;
     }
+
+    int i;
+    for (i = 0; i < NR_CMD; i ++) {
+      if (strcmp(cmd, cmd_table[i].name) == 0) {
+        if (cmd_table[i].handler(args) < 0) { return; }
+        break;
+      }
+    }
+
+    if (i == NR_CMD) { printf("Unknown command '%s'\n", cmd); }
   }
+}
   
 int main(int argc, char **argv) {
     Verilated::commandArgs(argc, argv);
@@ -413,4 +413,3 @@ int main(int argc, char **argv) {
 }
 
 
-  
