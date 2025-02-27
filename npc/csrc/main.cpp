33e591858d1ff94f4e4c3c710e0b65eb91db38f3
tracer-ysyx <tracer@ysyx.org>
2025-02-27 17:35:29 +0800

    > sim RTL ysyx_24120009 贾树傲 Linux LAPTOP-5GG1EBB7 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  17:35:29 up  2:48,  0 users,  load average: 0.22, 0.14, 0.10

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -19,8 +19,8 @@
 #define UART_ADDR_LEN  8          
 
 // #define ENABLE_MEMORY_CHECK 1
-#define DIFFTEST 1
-#define is_silent_mode 0
+// #define DIFFTEST 1
+#define is_silent_mode 1
 
 // Declare global variables
 Vysyx_24120009_core* top;  // Top module (global)
