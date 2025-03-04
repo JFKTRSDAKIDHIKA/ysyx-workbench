8fd2b88dcf7b8c8620bb8b6968b44bd85cba11ac
tracer-ysyx <tracer@ysyx.org>
2025-03-04 13:27:12 +0000

    > sim RTL ysyx_24120009 贾树傲 Linux Ubuntu 6.8.0-52-generic #53-Ubuntu SMP PREEMPT_DYNAMIC Sat Jan 11 00:06:25 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  13:27:12 up  7:41,  3 users,  load average: 0.51, 0.42, 0.30

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -277,7 +277,7 @@ static int execute_single_step() {
   tick(top, is_silent_mode);  
 #ifdef DIFFTEST
   if (need_check) {
-    printf("check once\n");
+    printf("check once, %d\n", top->io_wbu_state_debug);
     need_check = (top->io_wbu_state_debug == 2);
     ref_difftest_regcpy(&ref, DIFFTEST_TO_REF);
     ref_difftest_exec(1);
