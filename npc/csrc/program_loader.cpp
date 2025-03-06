1a18bebc539d646d6a1d7e00f1faa487b0b0b8a0
tracer-ysyx <tracer@ysyx.org>
2025-03-06 13:44:48 +0000

    > sim RTL ysyx_24120009 贾树傲 Linux Ubuntu 6.8.0-52-generic #53-Ubuntu SMP PREEMPT_DYNAMIC Sat Jan 11 00:06:25 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  13:44:48 up 52 min,  3 users,  load average: 0.28, 0.31, 0.25

--- npc/csrc/program_loader.cpp
+++ npc/csrc/program_loader.cpp
@@ -53,4 +53,44 @@ void load_program(const char *program_path) {
             break;
         }
     }
+
+    // Load .data.ans from elf to MROM
+    for (int i = 0; i < reader.sections.size(); ++i) {
+        ELFIO::section* sec = reader.sections[i];
+
+        if (sec->get_name() == ".data.ans") {
+            const char* code = sec->get_data();
+            if (code == nullptr) {
+                std::cerr << "Failed to get .data.ans section data!" << std::endl;
+            }
+
+            std::cout << "Loading .data.ans section to MROM: " << std::setw(8) << std::setfill('0') << std::hex << address <<std::endl;
+            for (int j = 0; j < sec->get_size(); ++j) {
+                // printf("%02X ", (unsigned char)code[j]);
+                Memory::pmem_write(address, code[j], 0x1);
+                address += sizeof(code[j]); 
+            }
+            break;
+        }
+    }
+
+    // Load .data.test_data from elf to MROM
+    for (int i = 0; i < reader.sections.size(); ++i) {
+        ELFIO::section* sec = reader.sections[i];
+
+        if (sec->get_name() == ".data.test_data") {
+            const char* code = sec->get_data();
+            if (code == nullptr) {
+                std::cerr << "Failed to get .data.test_data section data!" << std::endl;
+            }
+
+            std::cout << "Loading .data.test_data section to MROM: " << std::setw(8) << std::setfill('0') << std::hex << address <<std::endl;
+            for (int j = 0; j < sec->get_size(); ++j) {
+                // printf("%02X ", (unsigned char)code[j]);
+                Memory::pmem_write(address, code[j], 0x1);
+                address += sizeof(code[j]); 
+            }
+            break;
+        }
+    }
 }
