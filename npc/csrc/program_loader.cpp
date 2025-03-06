a4ad7676f5f641d66754f85a8076986f26673c69
tracer-ysyx <tracer@ysyx.org>
2025-03-06 10:54:34 +0000

    > sim RTL ysyx_24120009 贾树傲 Linux Ubuntu 6.8.0-52-generic #53-Ubuntu SMP PREEMPT_DYNAMIC Sat Jan 11 00:06:25 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  10:54:34 up  2:13,  2 users,  load average: 0.43, 0.30, 0.22

--- npc/csrc/program_loader.cpp
+++ npc/csrc/program_loader.cpp
@@ -6,7 +6,6 @@
 
 
 void load_program(const char *program_path) {
-
     ELFIO::elfio reader;
 
     if (!reader.load(program_path)) {
@@ -17,24 +16,18 @@ void load_program(const char *program_path) {
         ELFIO::section* sec = reader.sections[i];
 
         if (sec->get_name() == ".text") {
-            std::cout << "Found .text section!" << std::endl;
-            std::cout << "Size: " << sec->get_size() << " bytes" << std::endl;
-
             const char* code = sec->get_data();
             if (code == nullptr) {
                 std::cerr << "Failed to get .text section data!" << std::endl;
             }
 
             uint32_t address = 0x20000000; 
-
             for (int j = 0; j < sec->get_size(); ++j) {
                 // printf("%02X ", (unsigned char)code[j]);
                 Memory::pmem_write(address, code[j], 0x1);
                 address += sizeof(code[j]); 
             }
-            std::cout << std::endl;
             break;
         }
     }
-
 }
