d2d5efd8ce16d81aa4c2148caeb0511f1a0c9b3f
tracer-ysyx <tracer@ysyx.org>
2025-03-06 13:39:12 +0000

    > sim RTL ysyx_24120009 贾树傲 Linux Ubuntu 6.8.0-52-generic #53-Ubuntu SMP PREEMPT_DYNAMIC Sat Jan 11 00:06:25 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  13:39:12 up 47 min,  3 users,  load average: 0.32, 0.21, 0.17

--- npc/csrc/program_loader.cpp
+++ npc/csrc/program_loader.cpp
@@ -12,6 +12,8 @@ void load_program(const char *program_path) {
         std::cerr << "Failed to load ELF file: " << program_path << std::endl;
     }
 
+    uint32_t address = 0x20000000; 
+
     for (int i = 0; i < reader.sections.size(); ++i) {
         ELFIO::section* sec = reader.sections[i];
 
@@ -21,7 +23,27 @@ void load_program(const char *program_path) {
                 std::cerr << "Failed to get .text section data!" << std::endl;
             }
 
-            uint32_t address = 0x20000000; 
+            std::cout << "Loading .text section to MROM: " << address <<std::endl;
+            for (int j = 0; j < sec->get_size(); ++j) {
+                // printf("%02X ", (unsigned char)code[j]);
+                Memory::pmem_write(address, code[j], 0x1);
+                address += sizeof(code[j]); 
+            }
+            break;
+        }
+    }
+
+    // Load .rodata from elf to MROM
+    for (int i = 0; i < reader.sections.size(); ++i) {
+        ELFIO::section* sec = reader.sections[i];
+
+        if (sec->get_name() == ".rodata") {
+            const char* code = sec->get_data();
+            if (code == nullptr) {
+                std::cerr << "Failed to get .rodata section data!" << std::endl;
+            }
+
+            std::cout << "Loading .rodata section to MROM: " << address <<std::endl;
             for (int j = 0; j < sec->get_size(); ++j) {
                 // printf("%02X ", (unsigned char)code[j]);
                 Memory::pmem_write(address, code[j], 0x1);
