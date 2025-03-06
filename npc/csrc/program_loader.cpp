2923d05c4457e96aa3f9ade6d342048b976fec01
tracer-ysyx <tracer@ysyx.org>
2025-03-06 13:52:49 +0000

    > sim RTL ysyx_24120009 贾树傲 Linux Ubuntu 6.8.0-52-generic #53-Ubuntu SMP PREEMPT_DYNAMIC Sat Jan 11 00:06:25 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  13:52:49 up  1:00,  3 users,  load average: 0.33, 0.20, 0.19

--- npc/csrc/program_loader.cpp
+++ npc/csrc/program_loader.cpp
@@ -15,7 +15,7 @@ void load_program(const char *program_path) {
     }
 
     uint32_t address = 0x20000000; 
-    std::cout << "End of .text section: 0x" << std::setw(8) << std::setfill('0') << std::hex << (void*)&etext <<std::endl;
+    std::cout << "End of .text section: " << std::setw(8) << std::setfill('0') << std::hex << etext <<std::endl;
 
     for (int i = 0; i < reader.sections.size(); ++i) {
         ELFIO::section* sec = reader.sections[i];
