297e9b61926c0f6a79e500a93263d565c11721e9
tracer-ysyx <tracer@ysyx.org>
2025-03-07 03:52:02 +0000

    > sim RTL ysyx_24120009 贾树傲 Linux Ubuntu 6.8.0-52-generic #53-Ubuntu SMP PREEMPT_DYNAMIC Sat Jan 11 00:06:25 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  03:52:02 up  2:13,  2 users,  load average: 1.56, 1.48, 1.61

--- npc/csrc/program_loader.cpp
+++ npc/csrc/program_loader.cpp
@@ -44,7 +44,28 @@ void load_program(const char *program_path) {
                 std::cerr << "Failed to get .data section data!" << std::endl;
             }
 
-            std::cout << "Loading .data section to MROM: " << std::setw(8) << std::setfill('0') << std::hex << address <<std::endl;
+            std::cout << "Loading .data section to MROM: 0x" << std::setw(8) << std::setfill('0') << std::hex << address <<std::endl;
+            for (int j = 0; j < sec->get_size(); ++j) {
+                // printf("%02X ", (unsigned char)code[j]);
+                Memory::pmem_write(address, code[j], 0x1);
+                address += sizeof(code[j]); 
+            }
+            break;
+        }
+    }
+
+    
+    // Load .bbs from elf to MROM
+    for (int i = 0; i < reader.sections.size(); ++i) {
+        ELFIO::section* sec = reader.sections[i];
+
+        if (sec->get_name() == ".bbs") {
+            const char* code = sec->get_data();
+            if (code == nullptr) {
+                std::cerr << "Failed to get .bbs section data!" << std::endl;
+            }
+
+            std::cout << "Loading .bbs section to MROM: 0x" << std::setw(8) << std::setfill('0') << std::hex << address <<std::endl;
             for (int j = 0; j < sec->get_size(); ++j) {
                 // printf("%02X ", (unsigned char)code[j]);
                 Memory::pmem_write(address, code[j], 0x1);
