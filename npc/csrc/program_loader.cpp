fd643af051a0dfaade88e959fba04983c8c0588e
tracer-ysyx <tracer@ysyx.org>
2025-03-07 03:47:27 +0000

    > sim RTL ysyx_24120009 贾树傲 Linux Ubuntu 6.8.0-52-generic #53-Ubuntu SMP PREEMPT_DYNAMIC Sat Jan 11 00:06:25 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  03:47:27 up  2:08,  2 users,  load average: 2.13, 1.22, 1.58

--- npc/csrc/program_loader.cpp
+++ npc/csrc/program_loader.cpp
@@ -34,37 +34,17 @@ void load_program(const char *program_path) {
         }
     }
 
-    // Load .data.ans from elf to MROM
+    // Load .data from elf to MROM
     for (int i = 0; i < reader.sections.size(); ++i) {
         ELFIO::section* sec = reader.sections[i];
 
-        if (sec->get_name() == ".data.ans") {
+        if (sec->get_name() == ".data") {
             const char* code = sec->get_data();
             if (code == nullptr) {
-                std::cerr << "Failed to get .data.ans section data!" << std::endl;
+                std::cerr << "Failed to get .data section data!" << std::endl;
             }
 
-            std::cout << "Loading .data.ans section to MROM: " << std::setw(8) << std::setfill('0') << std::hex << address <<std::endl;
-            for (int j = 0; j < sec->get_size(); ++j) {
-                // printf("%02X ", (unsigned char)code[j]);
-                Memory::pmem_write(address, code[j], 0x1);
-                address += sizeof(code[j]); 
-            }
-            break;
-        }
-    }
-
-    // Load .data.test_data from elf to MROM
-    for (int i = 0; i < reader.sections.size(); ++i) {
-        ELFIO::section* sec = reader.sections[i];
-
-        if (sec->get_name() == ".data.test_data") {
-            const char* code = sec->get_data();
-            if (code == nullptr) {
-                std::cerr << "Failed to get .data.test_data section data!" << std::endl;
-            }
-
-            std::cout << "Loading .data.test_data section to MROM: " << std::setw(8) << std::setfill('0') << std::hex << address <<std::endl;
+            std::cout << "Loading .data section to MROM: " << std::setw(8) << std::setfill('0') << std::hex << address <<std::endl;
             for (int j = 0; j < sec->get_size(); ++j) {
                 // printf("%02X ", (unsigned char)code[j]);
                 Memory::pmem_write(address, code[j], 0x1);
