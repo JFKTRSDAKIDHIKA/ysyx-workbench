e808393ca1e2d251db8543b4fadcb24c960f4443
tracer-ysyx <tracer@ysyx.org>
2025-03-07 07:57:24 +0000

    > run NEMU ysyx_24120009 贾树傲 Linux Ubuntu 6.8.0-52-generic #53-Ubuntu SMP PREEMPT_DYNAMIC Sat Jan 11 00:06:25 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  07:57:24 up  2:13,  3 users,  load average: 0.30, 2.31, 2.36

--- abstract-machine/am/src/riscv/npc/trm.c
+++ abstract-machine/am/src/riscv/npc/trm.c
@@ -8,7 +8,9 @@ extern char _heap_start;
 extern char _pmem_start;
 extern char _data_lma;    /* LMA（加载地址）来自 mrom */
 extern char _data;        /* VMA（运行时地址）在 sram */
-extern char _edata;      
+extern char _edata;    
+extern char _bss_start;  
+extern char __ebss;
 int main(const char *args);
 #define PMEM_END  ((uintptr_t)&_pmem_start + PMEM_SIZE)
 
@@ -24,16 +26,16 @@ void halt(int code) {
   while (1);
 }
 
-void copy_data() {
+void Bootloader() {
   uint32_t *src = (uint32_t *)&_data_lma;
   uint32_t *dst = (uint32_t *)&_data;
   uint32_t len = &_edata - &_data;
-
+  memset(_bss_start, 0, _ebss - _bss_start);
   memcpy((void *)dst, (void *)src, len);
 }
 
 void _trm_init() {
-  copy_data();
+  Bootloader();
   int ret = main(mainargs);
   halt(ret);
 }
