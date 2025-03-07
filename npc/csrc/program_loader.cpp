086733e93c08533c695f061db796f58ad910b352
tracer-ysyx <tracer@ysyx.org>
2025-03-07 02:07:19 +0000

    > sim RTL ysyx_24120009 贾树傲 Linux Ubuntu 6.8.0-52-generic #53-Ubuntu SMP PREEMPT_DYNAMIC Sat Jan 11 00:06:25 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  02:07:19 up 28 min,  2 users,  load average: 2.51, 1.82, 2.18

--- npc/csrc/program_loader.cpp
+++ npc/csrc/program_loader.cpp
@@ -5,8 +5,7 @@
 #include "include/program_loader.h"
 #include "ELFIO/elfio/elfio.hpp"
 
-extern char _etext;
-extern char __etext;
+extern char _etext asm("_etext");
 
 void load_program(const char *program_path) {
     ELFIO::elfio reader;
@@ -17,7 +16,6 @@ void load_program(const char *program_path) {
 
     uint32_t address = 0x20000000; 
     printf("End of .text section (_etext): %p\n", (void*)&_etext);
-    printf("__etext: %p\n", (void*)&__etext);
 
     for (int i = 0; i < reader.sections.size(); ++i) {
         ELFIO::section* sec = reader.sections[i];
