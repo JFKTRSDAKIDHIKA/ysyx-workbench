ee2b619fb971cf199c06cf777bb79ddcb49b0ee1
tracer-ysyx <tracer@ysyx.org>
2025-03-08 09:39:04 +0000

    > sim RTL ysyx_24120009 贾树傲 Linux Ubuntu 6.11.0-19-generic #19~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Feb 17 11:51:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  09:39:04 up  2:21,  2 users,  load average: 0.03, 0.07, 0.08

--- abstract-machine/am/src/riscv/npc/include/npc.h
+++ abstract-machine/am/src/riscv/npc/include/npc.h
@@ -2,7 +2,7 @@
 #define NPC_H__
 
 // USAR
-#define UART_BASE 0x10000000L
+#define UART_BASE 0x10000000
 #define UART_TX   (UART_BASE + 0x0)     // Transmitter Holding Register (THR)
 #define UART_LCR  (UART_BASE + 0x03)    // Line Control Register
 #define UART_DLL  (UART_BASE + 0x00)    // Divisor Latch Byte 1
