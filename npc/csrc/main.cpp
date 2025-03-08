867e382b04bdbadd02b7146e82b2ef6a8d209098
tracer-ysyx <tracer@ysyx.org>
2025-03-08 07:27:03 +0000

    > sim RTL ysyx_24120009 贾树傲 Linux Ubuntu 6.11.0-19-generic #19~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Feb 17 11:51:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  07:27:03 up 9 min,  2 users,  load average: 1.67, 3.14, 1.86

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -13,8 +13,8 @@
 #include <readline/history.h>
 #include <cassert>    
 
-#define ENABLE_MEMORY_CHECK 1
-#define DIFFTEST 1
+//#define ENABLE_MEMORY_CHECK 1
+//#define DIFFTEST 1
 #define is_silent_mode 1
 
 // Declare global variables
