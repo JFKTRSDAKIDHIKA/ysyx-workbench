19fe3915e7ca784ab3519c226cfa5af446a665f6
tracer-ysyx <tracer@ysyx.org>
2025-03-12 04:05:39 +0000

    > sim RTL ysyx_24120009 贾树傲 Linux Ubuntu 6.11.0-19-generic #19~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Feb 17 11:51:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  04:05:39 up  1:38,  2 users,  load average: 0.08, 0.22, 0.16

--- npc/obj_dir/VysyxSoCFull___024root__DepSet_h0188974b__0.cpp
+++ npc/obj_dir/VysyxSoCFull___024root__DepSet_h0188974b__0.cpp
@@ -2175,6 +2175,10 @@ VL_INLINE_OPT void VysyxSoCFull___024root___nba_sequent__TOP__7(VysyxSoCFull___0
                                                                    & (~ 
                                                                       ((IData)(vlSelf->ysyxSoCFull__DOT__asic__DOT__lspi__DOT__mspi__DOT__u0_spi_top__DOT__ctrl) 
                                                                        >> 0xdU)))))))));
+    VL_WRITEF("ss: %1#\nsck: %1#\n",1,(1U & ((IData)(vlSelf->ysyxSoCFull__DOT___asic_spi_ss) 
+                                             >> 7U)),
+              1,(IData)(vlSelf->ysyxSoCFull__DOT___asic_spi_sck));
+    Verilated::runFlushCallbacks();
     vlSelf->ysyxSoCFull__DOT____Vcellinp__flash__ss 
         = (1U & (IData)(vlSelf->ysyxSoCFull__DOT___asic_spi_ss));
 }
--- npc/vsrc/perip/bitrev/bitrev.v
+++ npc/vsrc/perip/bitrev/bitrev.v
@@ -14,6 +14,10 @@ module bitrev (
   reg [1:0]  state;
   wire inactive = ss;
 
+  always @(*) begin
+    $write("ss: %d\n", ss);
+    $write("sck: %d\n", sck);
+  end
 
   always @(posedge sck) begin
     if (inactive) begin 
