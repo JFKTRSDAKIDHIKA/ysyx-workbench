033af1f8af57c0e3a808ce119b316e2423bd0bfc
tracer-ysyx <tracer@ysyx.org>
2025-03-12 05:48:12 +0000

    > sim RTL ysyx_24120009 贾树傲 Linux Ubuntu 6.11.0-19-generic #19~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Feb 17 11:51:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  05:48:12 up  3:21,  3 users,  load average: 0.25, 0.16, 0.10

--- npc/obj_dir/VysyxSoCFull___024root__DepSet_h4d156741__0.cpp
+++ npc/obj_dir/VysyxSoCFull___024root__DepSet_h4d156741__0.cpp
@@ -4855,7 +4855,7 @@ VL_INLINE_OPT void VysyxSoCFull___024root___nba_sequent__TOP__5(VysyxSoCFull___0
                 ? 2U : (IData)(vlSelf->ysyxSoCFull__DOT__bitrev__DOT__state));
         vlSelf->ysyxSoCFull__DOT___bitrev_miso = (1U 
                                                   & ((IData)(vlSelf->ysyxSoCFull__DOT__bitrev__DOT__data_in) 
-                                                     >> 6U));
+                                                     >> 7U));
         vlSelf->__Vdly__ysyxSoCFull__DOT__bitrev__DOT__data_in 
             = (0xfeU & ((IData)(vlSelf->ysyxSoCFull__DOT__bitrev__DOT__data_in) 
                         << 1U));
