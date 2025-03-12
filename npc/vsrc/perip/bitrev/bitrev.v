72eb5b68563e6c7bb21b8e17d8ffa094e0a51a75
tracer-ysyx <tracer@ysyx.org>
2025-03-12 05:20:23 +0000

    > sim RTL ysyx_24120009 贾树傲 Linux Ubuntu 6.11.0-19-generic #19~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Feb 17 11:51:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  05:20:23 up  2:53,  3 users,  load average: 0.26, 0.12, 0.09

--- npc/vsrc/perip/bitrev/bitrev.v
+++ npc/vsrc/perip/bitrev/bitrev.v
@@ -41,7 +41,7 @@ module bitrev (
         TX: begin
           $write("TX\n");
           $write("data: %b\n", data_in);
-          miso <= data_in[7];
+          miso <= data_in[6];
           $write("miso: %d\n", miso);
           data_in <= {data_in[6:0], 1'b0};
           counter <= (counter < 8'd7 ) ? counter + 8'd1 : 8'd0;
