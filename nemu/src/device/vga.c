ac1e272687de71628799291d7d8b5166184a967e
tracer-ysyx <tracer@ysyx.org>
2025-03-13 20:52:42 +0800

    > run NEMU ysyx_24120009 贾树傲 Linux LAPTOP-5GG1EBB7 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  20:52:42 up  1:14,  0 users,  load average: 1.22, 0.52, 0.34

--- nemu/src/device/vga.c
+++ nemu/src/device/vga.c
@@ -47,7 +47,7 @@ static void init_screen() {
   sprintf(title, "%s-NEMU", str(__GUEST_ISA__));
   SDL_Init(SDL_INIT_VIDEO);  
   SDL_CreateWindowAndRenderer(
-      600,
+      500,
       500,
       0, &window, &renderer);
   SDL_SetWindowTitle(window, title);
