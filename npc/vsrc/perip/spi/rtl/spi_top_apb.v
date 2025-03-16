a0d6ca8e11a6d8ded737697effc771a56eb7fed6
tracer-ysyx <tracer@ysyx.org>
2025-03-16 05:35:13 +0000

    > sim RTL ysyx_24120009 贾树傲 Linux Ubuntu 6.11.0-19-generic #19~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Feb 17 11:51:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  05:35:13 up  1:53,  2 users,  load average: 0.27, 0.14, 0.06

--- npc/vsrc/perip/spi/rtl/spi_top_apb.v
+++ npc/vsrc/perip/spi/rtl/spi_top_apb.v
@@ -103,6 +103,7 @@ always @(posedge clock or posedge reset) begin
       IDLE: begin 
         if (is_flash_access && in_psel && in_penable) begin
           state <= SEND_CMD;
+          $write("hhhhh\n");
         end else if (is_spi_master_access && in_psel && in_penable) begin
           state <= DONE; // Directly pass through for SPI master access
         end
