612e13e7d5f4401b0f4bfa8b36e49cae5afbf45c
tracer-ysyx <tracer@ysyx.org>
2025-03-16 07:43:08 +0000

    > sim RTL ysyx_24120009 贾树傲 Linux Ubuntu 6.11.0-19-generic #19~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Feb 17 11:51:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  07:43:08 up  4:01,  2 users,  load average: 0.31, 0.19, 0.19

--- npc/vsrc/perip/spi/rtl/spi_top_apb.v
+++ npc/vsrc/perip/spi/rtl/spi_top_apb.v
@@ -259,15 +259,16 @@ always @(posedge clock or posedge reset) begin
       DONE: begin
         // Transaction done
         flash_wb_ack_o <= 1'b1;
-        flash_wb_adr_i <= 5'b0;
-        flash_wb_dat_i <= 32'b0;
-        flash_wb_sel_i <= 4'b0;
-        flash_wb_we_i <= 1'b0;
-        flash_wb_stb_i <= 1'b0;
-        flash_wb_cyc_i <= 1'b0;
-        flash_wb_ack_o <= 1'b0;
-        if (!in_penable) 
+        if (!in_penable && !in_psel) begin
           state <= IDLE;
+          flash_wb_adr_i <= 5'b0;
+          flash_wb_dat_i <= 32'b0;
+          flash_wb_sel_i <= 4'b0;
+          flash_wb_we_i <= 1'b0;
+          flash_wb_stb_i <= 1'b0;
+          flash_wb_cyc_i <= 1'b0;
+          flash_wb_ack_o <= 1'b0;
+        end
         else
           state <= state;
         end
