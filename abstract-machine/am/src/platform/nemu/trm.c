0d5a3469bb39682aeaa1b24c91aaf579cb984fdf
tracer-ysyx <tracer@ysyx.org>
2025-03-16 23:56:36 +0800

    > run NEMU ysyx_24120009 贾树傲 Linux LAPTOP-5GG1EBB7 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  23:56:36 up 1 day, 16:15,  0 users,  load average: 1.03, 1.00, 1.00

--- abstract-machine/am/src/platform/nemu/trm.c
+++ abstract-machine/am/src/platform/nemu/trm.c
@@ -1,14 +1,7 @@
 #include <am.h>
 #include <nemu.h>
-#include <klib.h>
 
 extern char _heap_start;
-extern char _data_lma;    /* LMA（加载地址）来自 mrom */
-extern char _data;        /* VMA（运行时地址）在 sram */
-extern char _edata;    
-extern char _ebss;
-extern char _bss_start;
-
 int main(const char *args);
 
 Area heap = RANGE(&_heap_start, PMEM_END);
@@ -25,22 +18,8 @@ void halt(int code) {
   while (1);
 }
 
-void crt0_init() {
-  // Copy .data section
-  if (&_edata > &_data) {
-    size_t data_size = (uintptr_t)&_edata - (uintptr_t)&_data;
-    memcpy(&_data, &_data_lma, data_size);
-  }
-
-  // Zero out .bss section
-  if (&_ebss > &_bss_start) {
-    size_t bss_size = (uintptr_t)&_ebss - (uintptr_t)&_bss_start;
-    memset(&_bss_start, 0, bss_size);
-  }
-}
 
 void _trm_init() {
-  crt0_init();
   int ret = main(mainargs);
   halt(ret);
 }
