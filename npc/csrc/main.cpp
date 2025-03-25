ba74d8c26d84af31f0282c1522644f86178a102f
tracer-ysyx <tracer@ysyx.org>
2025-03-25 20:30:50 +0800

    > sim RTL ysyx_24120009 贾树傲 Linux LAPTOP-5GG1EBB7 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  20:30:50 up  6:16,  0 users,  load average: 0.36, 0.25, 0.34

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -112,7 +112,7 @@ int check_dut_and_ref(VysyxSoCFull* top, paddr_t start_addr, size_t size) {
           return -1;  
       }
   }
-
+/*
   // Compare program counter (PC) between DUT and REF
   if (top->io_pc_debug != ref.pc) {
       std::cerr << "PC mismatch - DUT: 0x" << std::hex << top->io_pc_debug 
@@ -130,6 +130,7 @@ int check_dut_and_ref(VysyxSoCFull* top, paddr_t start_addr, size_t size) {
 
       return -1;  // End simulation
   }
+*/
 /*
 #ifdef ENABLE_MEMORY_CHECK
   // ----------- 检查内存 -----------
