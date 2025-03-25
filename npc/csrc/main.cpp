ca644eea15ce0d47ef97be6572ec1a269f594eed
tracer-ysyx <tracer@ysyx.org>
2025-03-25 20:46:08 +0800

    > sim RTL ysyx_24120009 贾树傲 Linux LAPTOP-5GG1EBB7 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  20:46:08 up  6:31,  0 users,  load average: 1.15, 1.12, 0.72

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -15,7 +15,7 @@
 #include <cassert>    
 
 // #define ENABLE_MEMORY_CHECK 1
-#define DIFFTEST 1
+// #define DIFFTEST 1
 #define is_silent_mode 0
 #define TRACE
 
@@ -250,21 +250,15 @@ static char* rl_gets() {
   }
 
 static int need_check = 0;
-static int need_quit = 0;
 static int execute_single_step() {
   tick(top, is_silent_mode);  
 #ifdef DIFFTEST
   if (need_check) {
-    if (need_quit) {
-      return -1;
-    }
     need_check = (top->io_wbu_state_debug == 2);
     ref_difftest_exec(1);
     ref_difftest_regcpy(&ref, DIFFTEST_TO_REF);
-    need_quit = check_dut_and_ref(top, 0, 0);
-    return 0;
+    return check_dut_and_ref(top, 0, 0);
   } else {
-    need_quit = 0;
     need_check = (top->io_wbu_state_debug == 2);
     return 0;
   }
