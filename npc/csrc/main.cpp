4294683d94d2c23624031af9135e2b2f513c0098
tracer-ysyx <tracer@ysyx.org>
2025-03-25 20:42:47 +0800

    > sim RTL ysyx_24120009 贾树傲 Linux LAPTOP-5GG1EBB7 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  20:42:47 up  6:28,  0 users,  load average: 0.09, 0.35, 0.42

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -250,14 +250,19 @@ static char* rl_gets() {
   }
 
 static int need_check = 0;
+static int need_quit = 0;
 static int execute_single_step() {
   tick(top, is_silent_mode);  
 #ifdef DIFFTEST
   if (need_check) {
+    if (need_quit) {
+      return -1;
+    }
     need_check = (top->io_wbu_state_debug == 2);
     ref_difftest_exec(1);
     ref_difftest_regcpy(&ref, DIFFTEST_TO_REF);
-    return check_dut_and_ref(top, 0, 0);
+    need_quit = check_dut_and_ref(top, 0, 0);
+    return 0;
   } else {
     need_check = (top->io_wbu_state_debug == 2);
     return 0;
