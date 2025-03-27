2f037339900018632081e8de6885aadc3174f3c5
tracer-ysyx <tracer@ysyx.org>
2025-03-27 13:40:26 +0800

    > sim RTL ysyx_24120009 贾树傲 Linux LAPTOP-5GG1EBB7 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  13:40:26 up 10:46,  0 users,  load average: 0.00, 0.07, 0.13

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -51,7 +51,7 @@ void init_sdram(int instance_id) {
 // note: extern "C" 是 C++ 中的一个声明方式，用来告诉编译器，函数使用 C 的链接方式，而不是 C++ 默认的链接方式。
 extern "C" void flash_read(int32_t addr, int32_t *data) {
   *data = Memory::pmem_read(addr + FLASH_BASE_ADDR); 
-  // std::cout << "[DEBUG] Flash read "<< "addr: " << std::hex << addr << ", data: " << std::hex << *data << std::endl;
+  std::cout << "[DEBUG] Flash read "<< "addr: " << std::hex << addr << ", data: " << std::hex << *data << std::endl;
 }
 
 extern "C" void mrom_read(int32_t addr, int32_t *data) { 
