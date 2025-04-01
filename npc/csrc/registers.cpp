66cf3f19c30b922f631194003cc51f06c97de6ac
tracer-ysyx <tracer@ysyx.org>
2025-04-01 19:01:39 +0800

    > sim RTL ysyx_24120009 贾树傲 Linux LAPTOP-5GG1EBB7 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  19:01:39 up  1:18,  0 users,  load average: 0.16, 0.43, 0.52

--- npc/csrc/registers.cpp
+++ npc/csrc/registers.cpp
@@ -25,20 +25,22 @@ void print_register_values() {
     std::cout << "| Register Information              |" << std::endl;
     std::cout << "--------------------------------------" << std::endl;
 
-    std::cout << std::left << std::setw(6) << "Reg"   
+    std::cout << std::left 
+              << std::setw(6) << "Reg"   
               << std::setw(6) << "Name"  
-              << std::setw(12) << "Value (HEX)" << std::endl;
+              << std::setw(12) << "Value (HEX)" 
+              << std::endl;
+
     std::cout << "--------------------------------------" << std::endl;
 
-    // Output the value of each register
     for (int i = 0; i < 32; ++i) {
         std::cout << std::left << std::setw(6) << ("x" + std::to_string(i)) 
                   << std::setw(6) << regs[i]   
-                  << "0x" << std::uppercase << std::setfill('0') 
-                  << std::setw(8) << std::hex << rf_values[i] 
+                  << "0x" << std::uppercase 
+                  << std::setw(8) << std::setfill('0') << std::hex << rf_values[i]
+                  << std::setfill(' ')  
                   << std::endl;
     }
     
     std::cout << "--------------------------------------" << std::endl;
 }
-
