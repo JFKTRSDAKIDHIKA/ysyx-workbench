556e8b4bd593423afa5f085fb85911d7bbcd5efc
tracer-ysyx <tracer@ysyx.org>
2025-04-08 15:05:28 +0800

    > run NEMU ysyx_24120009 贾树傲 Linux LAPTOP-5GG1EBB7 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  15:05:28 up 48 min,  0 users,  load average: 0.27, 0.26, 0.20

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -34,7 +34,7 @@ bool cte_init(Context*(*handler)(Event, Context*)) {
 Context *kcontext(Area kstack, void (*entry)(void *), void *arg) {
   Context *c = (Context *)((uintptr_t)kstack.end - sizeof(Context));
   c->mepc = (uintptr_t)entry;
-  c->gpr[2] = (uintptr_t)kstack.end;
+  c->gpr[2] = (uintptr_t)kstack.end - sizeof(Context);
   c->gpr[10] = (uintptr_t)arg;  
   return c;
 }
