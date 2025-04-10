b10715dce7c20ea46a65e2c2970d5fdfce83088d
tracer-ysyx <tracer@ysyx.org>
2025-04-10 11:53:46 +0800

    > sim RTL ysyx_24120009 贾树傲 Linux LAPTOP-5GG1EBB7 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  11:53:46 up  1:49,  0 users,  load average: 0.33, 0.22, 0.13

--- abstract-machine/am/src/riscv/npc/trm.c
+++ abstract-machine/am/src/riscv/npc/trm.c
@@ -27,6 +27,8 @@ void second_stage_boot_loader() {
   // External symbols
   extern char _etext, _text, _text_lma;
   extern char _edata, _data, _data_lma;
+  extern char _erodata, _rodata, _rodata_lma;
+  extern char _edata_extra, _data_extra, _data_extra_lma;
   extern int main(const char *args);
 
   // Copy .text section
@@ -52,11 +54,57 @@ void second_stage_boot_loader() {
     }
   }
 
+  // Copy .rodata section
+  if (&_erodata > &_rodata) {
+    uint32_t *src = (uint32_t *)&_rodata_lma;  // Source addr in Flash
+    uint32_t *dst = (uint32_t *)&_rodata;      // Target addr in PSRAM 
+    unsigned long rodata_size = (uintptr_t)&_erodata - (uintptr_t)&_rodata;
+    
+    // Copy in 4-byte chunks
+    unsigned long word_count = rodata_size / 4;
+    while (word_count--) {
+        *dst++ = *src++;
+    }
+    
+    // Handle remaining bytes (if size not multiple of 4)
+    unsigned long remainder = rodata_size % 4;
+    if (remainder) {
+        char *byte_src = (char *)src;
+        char *byte_dst = (char *)dst;
+        while (remainder--) {
+            *byte_dst++ = *byte_src++;
+        }
+    }
+  }
+
   // Copy .data section
   if (&_edata > &_data) {
     uint32_t *src = (uint32_t *)&_data_lma;
     uint32_t *dst = (uint32_t *)&_data;
     unsigned long data_size = (uintptr_t)&_edata - (uintptr_t)&_data;
+    
+    // Copy in 4-byte chunks
+    unsigned long word_count = data_size / 4;
+    while (word_count--) {
+        *dst++ = *src++;
+    }
+    
+    // Handle remaining bytes (if size not multiple of 4)
+    unsigned long remainder = data_size % 4;
+    if (remainder) {
+        char *byte_src = (char *)src;
+        char *byte_dst = (char *)dst;
+        while (remainder--) {
+            *byte_dst++ = *byte_src++;
+        }
+    }
+  }
+
+  // Copy .data.extra section
+  if (&_edata_extra > &_data_extra) {
+    uint32_t *src = (uint32_t *)&_data_extra_lma;
+    uint32_t *dst = (uint32_t *)&_data_extra;
+    unsigned long data_size = (uintptr_t)&_edata_extra - (uintptr_t)&_data_extra;
     
     // Copy in 4-byte chunks
     unsigned long word_count = data_size / 4;
